95318e5d26777c636c6f2aabf83f15b6b46e35e0
sysu18364107-xiaoxiao-xiaoqi <58425129+sysu18364107-xiaoxiao-xiaoqi@users.noreply.github.com>
2021-02-01 18:07:35 +0800

    Create 04 define_const.c

---
+++ 04 define_const.c
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#define day 7
+int main()
+{
+	//变量的定义
+//语法：数据类型  变量名 = 初始值
+	int a = 10;
+	cout << "all day: " << day<<endl;
+	const int month = 12;
+	cout << "all month: " << month << endl;
+	system("pause");
+	return 0;
+}
